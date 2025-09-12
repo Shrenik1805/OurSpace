b60375f0ca2927e49046d7b89d7a3cedc0c6aad5
Shrenik1805 <149351957+Shrenik1805@users.noreply.github.com>
2025-09-12 09:42:15 +0530

    Update theme-provider.tsx

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -23,7 +23,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 export function ThemeProvider({
   children,
   defaultTheme = "system",
-  storageKey = "vite-ui-theme",
+  storageKey = "hello-love-ui-theme",
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
